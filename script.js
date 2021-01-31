fcc2a23800d92939cc5bd01c2e2e4d08b4c1131e
subash <subash@u6da05197b12158.ant.amazon.com>
2021-01-30 18:04:09 -0800

    v1 checkin

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 const lan = "en"
+const GOOGLE_TTS_URL = "https://texttospeech.googleapis.com/v1beta1/text:synthesize?key="
+const API_KEY = "AIzaSyDCUVKNJBzW6XAWg0K0mZCM9uMkE25SqDc"
+const OWLBOT_URL = "https://owlbot.info/api/v4/dictionary/"
+const OWLBOT_API_TOKEN = "da037b8ade60b09902c70611dc9fe12fb0ad8235"
+
 var lastWord = ""
+
+var currentList = []
+var misspelledList = []
+var currentIndex = 0
+var incorrectList = []
+var isIncorrectListSession = false
+
+if(typeof localStorage.incorrectList !== "undefined") {
+    incorrectList = JSON.parse(localStorage.incorrectList || []);
+}
+
+if(typeof localStorage.practiceList !== "undefined") {
+    currentList = JSON.parse(localStorage.practiceList || [])
+}
+
+// Auto navigate to practice
+var hash = window.location.hash;
+switch(hash){
+    case '#practice':
+        navigate("practice")
+        break;
+    case '#pronounce':
+        navigate("pronounce")
+        break;
+    case '#words':
+        navigate("wordlist")
+        break;
+    default:
+        //do nothing
+}
+
+hideButtonsBasedOnState()
 
 function speak() {
 
@@ -27,8 +64,46 @@ function speak() {
     }
 }
 
+function repeat() {
+    var word = $("#practice-word").text()
+    if (lastWord != word) {
+        lastWord = word
+        practiceWordMeaning(word)
+        textToSpeech(word)
+    } else {
+        console.log("Skipping fetch, playing same word")
+        audio.play()
+    }
+}
+
+function practiceWordMeaning(word) {
+    var url = OWLBOT_URL + word;
+
+    var params = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Token ' + OWLBOT_API_TOKEN
+        }
+    }
+
+    fetch(url, params)
+        .then((res) => res.json()
+            .then((result) => {
+                const obj = result
+
+                if(typeof obj.definitions === "undefined") {
+                    $("#practice-word-meaning").text("No definitions found for : " + word)
+                } else {
+                    $("#practice-word-pronunciation").text('/' + obj.pronunciation + '/')
+                    $("#practice-word-type").text(obj.definitions[0].type)
+                    $("#practice-word-meaning").html(obj.definitions[0].definition)
+                    $("#practice-word-example").html(obj.definitions[0].example)
+                }
+            }))
+}
+
 function textToSpeech(word) {
-    const url = 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=AIzaSyDCUVKNJBzW6XAWg0K0mZCM9uMkE25SqDc'
+    const url = GOOGLE_TTS_URL + API_KEY
     const data = {
         'input':{
             'text': word
@@ -62,12 +137,12 @@ function textToSpeech(word) {
 }
 
 function getWordMeaning(word) {
-    var url = "https://owlbot.info/api/v4/dictionary/" + word;
+    var url = OWLBOT_URL + word;
 
     var params = {
         method: 'GET',
         headers: {
-            'Authorization': 'Token ' + 'da037b8ade60b09902c70611dc9fe12fb0ad8235'
+            'Authorization': 'Token ' + OWLBOT_API_TOKEN
         }
     }
 
@@ -87,4 +162,184 @@ function getWordMeaning(word) {
                     $("#wordcard-example").html(obj.definitions[0].example)
                 }
             }))
+}
+
+function navigate(section) {
+    switch(section) {
+        case "pronounce":
+            $("#nav-pronounce").addClass("is-active")
+            $("#nav-practice").removeClass("is-active")
+            $("#nav-wordlist").removeClass("is-active")
+
+            $("#section-pronounce").removeClass("is-hidden")
+            $("#section-practice").addClass("is-hidden")
+            $("#section-wordlist").addClass("is-hidden")
+
+            window.location.hash = "pronounce"
+
+            break;
+        case "practice":
+            $("#nav-pronounce").removeClass("is-active")
+            $("#nav-practice").addClass("is-active")
+            $("#nav-wordlist").removeClass("is-active")
+
+            $("#section-pronounce").addClass("is-hidden")
+            $("#section-practice").removeClass("is-hidden")
+            $("#section-wordlist").addClass("is-hidden")
+
+            window.location.hash = "practice"
+
+            break;
+        case "wordlist":
+            $("#nav-pronounce").removeClass("is-active")
+            $("#nav-practice").removeClass("is-active")
+            $("#nav-wordlist").addClass("is-active")
+
+            $("#section-pronounce").addClass("is-hidden")
+            $("#section-practice").addClass("is-hidden")
+            $("#section-wordlist").removeClass("is-hidden")
+
+            window.location.hash = "words"
+
+            break;
+        default:
+            // Do nothing
+    }
+}
+
+function showMessage(message, type) {
+    if(type == "error"){
+        $("#message-text").text(message)
+        $("#message").addClass("is-danger")
+    } else {
+        $("#message-text").text(message)
+        $("#message").addClass("is-info")
+    }
+
+    $("#message").removeClass("is-hidden")
+    $(function() {
+        setTimeout(function() {
+            $("#message").addClass("is-hidden")
+            $("#message").removeClass("is-danger")
+            $("#message").removeClass("is-info")
+        }, 7000);
+    });
+}
+
+function loadFromWordList() {
+    if(!$("#wordlist").val()) {
+        currentList = []
+    } else {
+        currentList = $("#wordlist").val().split('\n');
+        localStorage.practiceList = JSON.stringify(currentList)
+    }
+}
+
+function loadFromMisspeltStorage() {
+    currentList = JSON.parse(localStorage.incorrectList)
+}
+
+function hideButtonsBasedOnState() {
+    if(typeof localStorage.currentItem !== "undefined") {
+        if(JSON.parse(localStorage.currentItem) > 0) {
+            $("#practice-resume").removeClass("is-hidden")
+        } else {
+            $("#practice-resume").addClass("is-hidden")
+        }
+    } else {
+        $("#practice-resume").addClass("is-hidden")
+    }
+    if(incorrectList.length > 0) {
+        $("#practice-miss").removeClass("is-hidden")
+    } else {
+        $("#practice-miss").addClass("is-hidden")
+    }
+}
+
+function startNewPractice(useMisspelledWords) {
+    if(useMisspelledWords) {
+        loadFromMisspeltStorage()
+        isIncorrectListSession = true
+    } else {
+        loadFromWordList()
+    }
+    misspelledList = []
+    currentIndex = 0
+
+    if(currentList.length < 1) {
+        showMessage("Practice words list is empty! Navigate to words section and select from file.", "error")
+    } else {
+        resumePractice(false)
+    }
+}
+
+function resumePractice(resume) {
+    $("#session-selector").addClass("is-hidden")
+    $("#practice-card").removeClass("is-hidden")
+
+    if(resume) {
+        currentIndex = JSON.parse(localStorage.currentItem)
+        console.log("Resuming previous sesion!")
+    }
+
+    if(currentIndex < currentList.length) {
+        $("#practice-word").text(currentList[currentIndex])
+        repeat()
+    }
+}
+
+function endPractice() {
+    $("#practice-card").addClass("is-hidden")
+    localStorage.currentItem = JSON.stringify(0)
+    localStorage.removeItem("practiceList")
+    isIncorrectListSession = false
+    hideButtonsBasedOnState()
+    $("#session-selector").removeClass("is-hidden")
+    window.location.hash = "practice"
+    window.location.reload()
+}
+
+function misspelledPractice() {
+    startNewPractice(true)
+}
+
+function next(result) {
+    if(result != true) {
+        appendToMissedList(currentList[currentIndex])
+    } else {
+        removeFromMissedList(currentList[currentIndex])
+    }
+    currentIndex++
+    if(currentIndex < currentList.length) {
+        if(!isIncorrectListSession) {
+            localStorage.currentItem = JSON.stringify(currentIndex)
+        }
+        $("#practice-word").text(currentList[currentIndex])
+        repeat()
+    } else {
+        $("#practice-word").text("")
+        $("#practice-word-pronunciation").text("")
+        $("#practice-word-type").text("")
+        $("#practice-word-meaning").html("End of word list! Click on End-practice to close.")
+        $("#practice-word-example").html("")
+
+        $("#repeat-button").hide()
+        $("#card-pass").hide()
+        $("#card-fail").hide()
+    }
+}
+
+function removeFromMissedList(word) {
+    incorrectList.splice($.inArray(word, incorrectList),1);
+    localStorage.incorrectList = JSON.stringify(incorrectList)
+    console.log(word + " removed from incorrect master list.");
+}
+
+function appendToMissedList(word) {
+    if(incorrectList.indexOf(word) === -1) {
+        incorrectList.push(word);
+        localStorage.incorrectList = JSON.stringify(incorrectList)
+        console.log(word + " added to incorrect master list.");
+    }
+    $("#miss-list").append("<p><i class='fas fa-times has-text-danger-dark'></i>&nbsp;&nbsp;<span>" + word + "</span></p>")
 }
